8f65de298a3302daceb013545aa5eaabb1d540d0
zhi.jiang <zhi.jiang@sectrend.com.cn>
2023-06-28 15:25:28 +0800

    fix: 优化类型

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -1,50 +1,55 @@
 export enum NodeTypes {
-  NumberLiteral = 'NumberLiteral',
-  Program = 'Program',
-  StringLiteral = 'StringLiteral',
-  CallExpression = 'CallExpression'
+  NumberLiteral = 'NumberLiteral', //数字字面量
+  Program = 'Program', //根字面量
+  StringLiteral = 'StringLiteral', //字符串字面量
+  CallExpression = 'CallExpression' //表达式字面量
 }
 
 export type ChildNode = NumberLiteralNode | CallExpressionNode | StringLiteralNode
 
-export interface Node {
+export type Node = {
   type: NodeTypes
 }
 
-export interface NumberLiteralNode extends Node {
+export type RootNode = Node & {
+  body: ChildNode[]
+  type: NodeTypes.Program
+  context?: ChildNode[]
+}
+export type NumberLiteralNode = Node & {
   type: NodeTypes.NumberLiteral
   value: string
 }
 
-export interface StringLiteralNode extends Node {
+export type StringLiteralNode = Node & {
   value: string
   type: NodeTypes.StringLiteral
 }
 
-export interface CallExpressionNode extends Node {
+export type CallExpressionNode = Node & {
   name: string
   params: ChildNode[]
   type: NodeTypes.CallExpression
   context?: ChildNode[]
 }
 
-export interface RootNode extends Node {
-  body: ChildNode[]
-  type: NodeTypes.Program
-  context?: ChildNode[]
+/**
+ * @function createRootNode 创建根节点
+ * @function createStringLiteralNode 创建字符节点
+ * @function createNumberLiteralNode 创建数字节点
+ * @function createCallExpressionNode 创建表达式节点
+ */
+export function createRootNode(): RootNode {
+  return {
+    type: NodeTypes.Program,
+    body: []
+  }
 }
 
 export function createStringLiteralNode(value: string): StringLiteralNode {
   return {
     type: NodeTypes.StringLiteral,
     value
-  }
-}
-
-export function createRootNode(): RootNode {
-  return {
-    type: NodeTypes.Program,
-    body: []
   }
 }
 
